f9b337d07c5f0e9261ad4dfc3e3a278c7e20d3b6
Glenn Barr <glenn@torchbox.com>
2015-05-07 10:23:57 +0100

    Migration conflict issue - merged

---
+++ torchbox/migrations/0010_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('torchbox', '0009_add_formpage_app'),
+        ('torchbox', '0009_auto_20150427_1230'),
+    ]
+
+    operations = [
+    ]
