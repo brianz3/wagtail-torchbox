ead0f4167e351f106f99dc4df805393d5127460f
Dave Cranwell <david@torchbox.com>
2015-06-15 14:58:47 +0100

    created merge which was apparently necessary

---
+++ torchbox/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('torchbox', '0011_auto_20150612_1126'),
+        ('torchbox', '0012_auto_20150611_1433'),
+    ]
+
+    operations = [
+    ]
